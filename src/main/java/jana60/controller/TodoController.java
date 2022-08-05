24fc23564dfa0eb4de90042e697a23da9f985f7d
costanzazanoletti <zanoletti.cos@gmail.com>
2022-08-05 12:57:31 +0200

    post put delete controllers

--- src/main/java/jana60/controller/TodoController.java
+++ src/main/java/jana60/controller/TodoController.java
@@ -2,11 +2,16 @@ package jana60.controller;
 
 import java.util.List;
 import java.util.Optional;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -31,6 +36,65 @@ public class TodoController {
     Optional<Todo> todo = repo.findById(id);
     if (todo.isPresent()) {
       return todo.get();
+    } else {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Todo not found");
+    }
+  }
+
+  @PostMapping
+  public Todo create(@Valid @RequestBody Todo todo) {
+    try {
+      return repo.save(todo);
+    } catch (Exception e) {
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Unable to save todo");
+    }
+  }
+
+  @DeleteMapping
+  public String delete() {
+    repo.deleteAll();
+    return "All todos deleted";
+  }
+
+  @DeleteMapping("/{id}")
+  public String deleteById(@PathVariable Integer id) {
+    Optional<Todo> todoToDelete = repo.findById(id);
+    if (todoToDelete.isPresent()) {
+      // lo cancello
+      repo.deleteById(id);
+      return "Todo with id " + id + " deleted";
+    } else {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Todo not found");
+    }
+  }
+
+  @PutMapping("/{id}")
+  public Todo update(@PathVariable Integer id, @Valid @RequestBody Todo todo) {
+    Optional<Todo> todoToUpdate = repo.findById(id);
+    if (todoToUpdate.isPresent()) {
+      // lo modifico
+      todo.setId(id);
+      return repo.save(todo);
+    } else {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Todo not found");
+    }
+  }
+
+  @GetMapping("/{id}/toggle")
+  public Todo toggleCompleted(@PathVariable Integer id) {
+    Optional<Todo> todoToToggle = repo.findById(id);
+    if (todoToToggle.isPresent()) {
+      // toggle completed
+      Todo todo = todoToToggle.get();
+      // if(todo.getCompleted()) {
+      // todo.setCompleted(false);
+      // } else {
+      // todo.setCompleted(true);
+      // }
+
+      todo.setCompleted(!todo.getCompleted());
+      return repo.save(todo);
+
     } else {
       throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Todo not found");
     }
--- src/main/java/jana60/model/Todo.java
+++ src/main/java/jana60/model/Todo.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 public class Todo {
@@ -12,6 +13,7 @@ public class Todo {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
 
+  @NotEmpty
   private String content;
 
   private Boolean completed;
